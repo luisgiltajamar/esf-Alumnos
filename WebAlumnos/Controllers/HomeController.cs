efb2668cbb5cd5a3da5e2ecbeddecc339b11744f
Luis Felipe Gil <luisgil@net2mate.com>
2015-03-04 17:08:03 +0100

    Primera subida

---
+++ WebAlumnos/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAlumnos.Models;
+
+namespace WebAlumnos.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            using (var db=new CursosEntities())
+            {
+                return View(db.Alumnos.ToList());
+
+            }
+        }
+
+        public ActionResult Alta()
+        {
+            return View(new Alumnos());
+        }
+
+        [HttpPost]
+        public ActionResult Alta(Alumnos model)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db=new CursosEntities())
+                {
+                    db.Alumnos.Add(model);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(model);
+        }
+
+    }
+}
