183bbb32177d33b843a8904679a279fe8edcd77e
Luis Felipe Gil <luisgil@net2mate.com>
2015-03-05 18:11:53 +0100

    D

--- WebAlumnos/Controllers/HomeController.cs
+++ WebAlumnos/Controllers/HomeController.cs
@@ -42,5 +42,16 @@ namespace WebAlumnos.Controllers
             return View(model);
         }
 
+        [HttpPost]
+        public ActionResult Buscar()
+        {
+            var bus = Request.Form["busqueda"];
+
+            var db = new CursosEntities();
+
+            var al = db.Alumnos.Where(o => o.apellidos.Contains(bus));
+            return View(al);
+        }
+
     }
 }
---
+++ WebAlumnos/Models/BuscadorAlumnos.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAlumnos.Models
+{
+    public  class BuscadorAlumnos
+    {
+
+        CursosEntities db=new CursosEntities();
+
+        public List<Alumnos> GetPorApellidos(String apellidos)
+        {
+
+            var datos = from o in db.Alumnos
+                where o.apellidos == apellidos && o.nombre=="Luis"
+                select o;
+
+            return datos.ToList();
+
+
+        }
+
+        public List<Alumnos> GetPorApellidosLamda(String apellidos)
+        {
+            
+
+            var datos = db.Alumnos.Where(o => o.apellidos == apellidos || o.nombre=="Luis" 
+                && o.Nacionalidades.nombre=="Ingles");
+
+            return datos.ToList();
+
+
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
